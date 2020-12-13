e3489d645fe716ec6113354de1d4dca5b436e7a6
nelya07 <75816118+nelya07@users.noreply.github.com>
2020-12-13 22:52:29 +0300

    Homework fix

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _speed;
     [SerializeField] private GameObject _template;
     [SerializeField] private float _coolDown = 0.5f;
     private float _lastShootAt = 0;
@@ -13,6 +15,7 @@ public class Player : MonoBehaviour
     {
         SpawnElements();
     }
+   
     private void SpawnElements()
     {
         GameObject instance = Instantiate(_template, transform.position + new Vector3(-2f, 2f, 0), Quaternion.identity);
@@ -21,6 +24,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        transform.position = new Vector3(47f, 1.5f, Mathf.Sin(Time.time * _speed) * 10f);
         if (Input.GetKey(KeyCode.Space) && Time.time - _lastShootAt >= _coolDown)
         {
             SpawnElements();
